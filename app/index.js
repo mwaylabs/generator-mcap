86e412025699f1331ea70bc9f8af3b5631bb728b
DrMabuse23 <p.brewing@mwaysolutions.com>
2015-01-22 19:04:01 +0100

    add client, server start entry , add directoryIndex on / off

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@
 var fs = require('fs');
 var path = require('path');
 var util = require('util');
-var util = require('util');
 var async = require('async');
 var yeoman = require('yeoman-generator');
 var uuid = require('node-uuid');
@@ -17,25 +16,34 @@ module.exports = yeoman.generators.Base.extend({
     this.uuid = uuid.v4();
     log.debug('Create project with uuid: %s', this.uuid);
 
-    this.argument('name', { type: String, required: false });
+    this.argument('name', {type: String, required: false});
     this.name = this.name || path.basename(process.cwd());
     this.name = this._.camelize(this._.slugify(this._.humanize(this.name)));
     this.baseAlias = '/' + this.name;
+    this.clientDocument = this.clientDocument;
+    this.serverDocument = this.serverDocument;
+    this.directoryIndex = this.directoryIndex;
   },
 
   prompting: function () {
     var done = this.async();
 
-    var prettyName = function(value) {
+    var prettyName = function (value) {
       return this._.camelize(this._.slugify(this._.humanize(value)));
     }.bind(this);
 
-    var parseAnswers = function(answers) {
+    var parseAnswers = function (answers) {
       this.name = answers.name;
       this.baseAlias = '/' + answers.baseAlias;
+      this.clientDocument = answers.clientDocument;
+      this.serverDocument = answers.serverDocument;
+      this.directoryIndex = answers.directoryIndex;
 
       log.debug('Name: %s', this.name);
       log.debug('BaseAlias: %s', this.baseAlias);
+      log.debug('ClientDocument: %s', this.clientDocument);
+      log.debug('ServerDocument: %s', this.serverDocument);
+      log.debug('DirectoryIndex: %s', this.directoryIndex);
 
       done();
     }.bind(this);
@@ -50,42 +58,74 @@ module.exports = yeoman.generators.Base.extend({
 
     // Show prompt to get values from user input
     var prompts = [{
-      name: 'name',
-      message: 'Project name',
-      validate: function(val) {
-        return val.length ? true : 'Please enter a project name';
-      }
-    },{
-      name: 'baseAlias',
-      message: 'Base alias',
-      default: function(props) {
-        return prettyName(props.name);
-      }.bind(this),
-      validate: function(value) {
-        return value.match(/^[/\w0-9-_]+$/) !== null ? true : 'Please enter a value without whitespace and special characters';
-      }
-    }];
-
+        name: 'name',
+        message: 'Project name',
+        validate: function (val) {
+          return val.length ? true : 'Please enter a project name';
+        }
+      }, {
+        name: 'baseAlias',
+        message: 'Base alias',
+        default: function (props) {
+          return prettyName(props.name);
+        }.bind(this),
+        validate: function (value) {
+          return value.match(/^[/\w0-9-_]+$/) !== null ? true : 'Please enter a value without whitespace and special characters';
+        }
+      },
+        {
+          name: 'clientDocument',
+          message: 'Client start Document',
+          default: './client/www/',
+          validate: function (value) {
+            return value.match(/^([a-zA-Z]:)?(\\{2}|\/)?([a-zA-Z0-9\\s_@-^!#$%&+={}\[\]]+(\\{2}|\/)?)|(\.(js|html)+)?$/) !== null ? true : 'Please enter a valid path';
+          }
+        },
+        {
+          name: 'serverDocument',
+          message: 'Server start Document',
+          default: './server/app.js',
+          validate: function (value) {
+            return value.match(/^([a-zA-Z]:)?(\\{2}|\/)?([a-zA-Z0-9\\s_@-^!#$%&+={}\[\]]+(\\{2}|\/)?)|(\.(js|html)+)?$/) !== null ? true : 'Please enter a valid path';
+          }
+        },
+        {
+          type: 'checkbox',
+          name: 'directoryIndex',
+          message: 'DirectoryIndex on/off',
+          choices: [
+            {
+              value: 'true',
+              name: 'yes',
+              checked: false
+            },
+            {
+              value: 'false',
+              name: 'no',
+              checked: true
+            }
+          ]
+        }
+      ];
     this.prompt(prompts, parseAnswers);
   },
 
-  // TODO move this function into a separate node module
-  _getProjectFolderName: function(name, cb) {
+// TODO move this function into a separate node module
+  _getProjectFolderName: function (name, cb) {
     var names = [name];
     var seperator = '-';
     var max = 100;
     for (var i = 1; i < max; i++) {
       names.push(util.format('%s%s%s', name, seperator, i));
     }
-    async.mapSeries(names, fs.stat, function(err) {
+    async.mapSeries(names, fs.stat, function (err) {
       cb(err.path);
     });
   },
-
   writing: {
     app: function () {
       var done = this.async();
-      this._getProjectFolderName(this.name, function(projectFolder){
+      this._getProjectFolderName(this.name, function (projectFolder) {
 
         this.dest.mkdir(projectFolder);
         log.debug('Create project folder: %s', projectFolder);
@@ -102,7 +142,6 @@ module.exports = yeoman.generators.Base.extend({
         done();
       }.bind(this));
     },
-
     projectfiles: function () {
       this.src.copy('editorconfig', '.editorconfig');
       this.src.copy('gitattributes', '.gitattributes');
@@ -111,7 +150,6 @@ module.exports = yeoman.generators.Base.extend({
       this.src.copy('mcapignore', '.mcapignore');
     }
   },
-
   end: function () {
     log.debug('generator-mcap is completed');
   }
